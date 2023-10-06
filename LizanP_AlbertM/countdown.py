9a23b771cf9e1ed462ddc06ffe78cc76893c1322
Albert Meng <meng0052@algonquinlive.com>
2023-10-06 00:31:50 -0400

    26-28 countdown

--- LizanP_AlbertM/countdown.py
+++ LizanP_AlbertM/countdown.py
@@ -3,11 +3,18 @@ def get_countdown_as_text_using_for(start_value=10):
     """Given a start_value, default is 10, return a space delimited string
        containing numbers descending from start_value to 0 using a for loop
     """
-    return ''
+    countdown=[]
+    for i in range(start_value,-1,-1):
+        countdown.append(i)
+    return countdown
 
 def get_countdown_as_text_using_while(start_value=10):
     """Given a start_value, default is 10, return a space delimited string
        containing numbers descending from start_value to 0 using a while loop
     """
-    return ''
+    countdown=[]
+    while(start_value>=0):
+       countdown.append(start_value)
+       start_value-=1
+    return countdown
 
