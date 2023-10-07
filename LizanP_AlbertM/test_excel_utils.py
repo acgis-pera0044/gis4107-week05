2553ed97e75d067f03d85042d35cc06379394585
Albert Meng <meng0052@algonquinlive.com>
2023-10-07 10:59:24 -0400

    small change to test_excel

--- LizanP_AlbertM/test_excel_utils.py
+++ LizanP_AlbertM/test_excel_utils.py
@@ -1,8 +1,15 @@
 import excel_utils as eu
 
-def test_create_cell_refs():
+def test_create_cell_refs1():
  columns = "A B"
  rows = "1 2"
  expection='A1,A2,B1,B2'
+ actual = eu.create_cell_refs(columns, rows)
+ assert expection==actual
+
+def test_create_cell_refs2():
+ columns = "A B C D"
+ rows = "1 2 3"
+ expection='A1,A2,A3,B1,B2,B3,C1,C2,C3,D1,D2,D3'
  actual = eu.create_cell_refs(columns, rows)
  assert expection==actual
